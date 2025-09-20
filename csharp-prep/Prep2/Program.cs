7d76eb0d3a98182b8124c155aa9765aff7313b86
BrantTanner <brant.tanner123@gmail.com>
2025-09-20 11:46:41 -0600

    cse210 prep#1-2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,48 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        string grade = "";
+
+        Console.Write("Please enter you grade percentage: ");
+        string valueFromUser = Console.ReadLine();
+
+        int gradePercent = int.Parse(valueFromUser);
+
+        if (gradePercent >= 90)
+        {
+            grade = "A";
+        }
+
+        else if (gradePercent >= 80)
+        {
+            grade = "B";
+        }
+
+        else if (gradePercent >= 70)
+        {
+            grade = "C";
+        }
+
+        else if (gradePercent >= 60)
+        {
+            grade = "D";
+        }
+
+        else
+        {
+            grade = "F";
+        }
+
+        Console.WriteLine($"Your grade is {grade}");
+
+        if (gradePercent >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+
+        else
+        {
+            Console.WriteLine("You failed...");
+        }
     }
 }
