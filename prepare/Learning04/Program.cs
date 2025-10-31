90c1fae0705e46a23e1e6552f785764a9d542d6f
BrantTanner <brant.tanner123@gmail.com>
2025-10-30 23:11:12 -0600

    learning04

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment assignment1 = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(assignment1.GetSummary());
+
+        MathAssignment mathAssignment = new MathAssignment("section 7.3", "Problems 8-19", "Robert Rodriguez", "Fractions");
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+
+        WritingAssignment writingAssignment = new WritingAssignment("The Causes of World War II by", "Mary Waters", "European History");
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInformation());
+
+
     }
 }
